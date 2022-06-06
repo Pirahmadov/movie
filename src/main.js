8fc7f4cd8b9bab45fe45f57920f6fc40b90a3710
Pirahmadov <orkhan.pirahmadov@gmail.com>
2022-06-06 23:14:24 +0400

    url elave eledim

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const movie = document.querySelector(".movie");
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  const url = "https://www.omdbapi.com/?apikey=trilogy&t=" + search.value;
+  fetch(url)
+    .then((result) => result.json())
+    .then((result) => {
+      const { Title, Year, Runtime, Actors, Poster } = result;
+      movie.innerHTML = Title;
+      document.querySelector(".year").innerHTML = Year;
+      document.querySelector(".runtime").innerHTML = Runtime;
+      document.querySelector(".actors").innerHTML = Actors;
+      document.querySelector("#poster").src = Poster;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
